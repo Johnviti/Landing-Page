d42a27bf6c96236af9fdaf23d000fe3811787729
John Victor <84483840+Johnviti@users.noreply.github.com>
2024-02-05 21:57:55 -0300

    Feat: Finalizando carrosel

--- Assets/Js/Carrosel.js
+++ Assets/Js/Carrosel.js
@@ -24,3 +24,25 @@ let swiperCards = new Swiper(".card__content", {
     },
   },
 });
+
+// let swiper = new Swiper('.swiper', {
+//   // Optional parameters
+//   direction: 'vertical',
+//   loop: true,
+
+//   // If we need pagination
+//   pagination: {
+//     el: '.swiper-pagination',
+//   },
+
+//   // Navigation arrows
+//   navigation: {
+//     nextEl: '.swiper-button-next',
+//     prevEl: '.swiper-button-prev',
+//   },
+
+//   // And if we need scrollbar
+//   scrollbar: {
+//     el: '.swiper-scrollbar',
+//   },
+// });
