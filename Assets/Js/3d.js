5842475082303990e0196bea4c82eeeda8cade3d
John Victor <84483840+Johnviti@users.noreply.github.com>
2024-03-03 02:37:02 -0300

    Feat: Adicionando mundo 3D e estilizando os textos do home

--- Assets/Js/3d.js
+++ Assets/Js/3d.js
@@ -5,8 +5,7 @@ import { OrbitControls } from "https://cdn.skypack.dev/three@0.129.0/examples/js
 import { GLTFLoader } from "https://cdn.skypack.dev/three@0.129.0/examples/jsm/loaders/GLTFLoader.js";
 
 const scene = new THREE.Scene();
-//câmera com posições e ângulos
-const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+const camera = new THREE.PerspectiveCamera(10, window.innerWidth / window.innerHeight, 0.1, 1000);
 
 const Spinner = document.getElementById("carregamento");
 const titulo = document.querySelector(".titulo");
@@ -16,14 +15,12 @@ const subttitulo = document.querySelector(".subtitulo");
 let mouseX = window.innerWidth / 2;
 let mouseY = window.innerHeight / 2;
 
-// Mantenha o objeto 3D em uma variável 
-let object;
 
-//permite que a câmera se mova ao redor da cena
+let object;
 let controls;
 
 
-let objToRender = 'world';
+let objToRender = 'World';
 
 const loader = new GLTFLoader();
 
@@ -31,7 +28,6 @@ const loader = new GLTFLoader();
 loader.load(
     `Assets/3D/${objToRender}/scene.gltf`,
     function (gltf) {
-        // Adicione o modelo 3D à cena
         object = gltf.scene;
         scene.add(object);
         Spinner.style.display = "none";
@@ -49,48 +45,37 @@ loader.load(
     }
 );
 
-// Instancie um novo renderizador e defina seu tamanho
 const renderer = new THREE.WebGLRenderer({ alpha: true }); // Alpha: true permite o fundo transparente
 renderer.setSize(window.innerWidth, window.innerHeight);
 
 // Adicione o renderizador ao DOM
 document.getElementById("container3D").appendChild(renderer.domElement);
 
-// Defina a distância da câmera para o modelo 3D
-camera.position.z = objToRender === "world" ? 15 : 500;
+camera.position.z = objToRender === "World" ? 12 : 500;
 
-// Adicione luzes à cena para que possamos realmente ver o modelo 3D
 const topLight = new THREE.DirectionalLight(0xffffff, 1); // (cor, intensidade)
-topLight.position.set(500, 500, 500) // canto superior esquerdo
+topLight.position.set(500, 500, 500) 
 topLight.castShadow = true;
 scene.add(topLight);
 
-const ambientLight = new THREE.AmbientLight(0x333333, objToRender === "world" ? 5 : 1);
+const ambientLight = new THREE.AmbientLight(0x333333, objToRender === "World" ? 5 : 1);
 scene.add(ambientLight);
 
 
-// Isso adiciona controles à câmera, para que possamos girá-la
-if (objToRender === "world") {
+if (objToRender === "World") {
     controls = new OrbitControls(camera, renderer.domElement);
-    controls.enableZoom = false; // Disable zooming
-
+    controls.enableZoom = false; 
 }
-// Isso adiciona controles à câmera, para que possamos girá-la / ampliá-la com o mouse
-// if (objToRender === "world") {
-//     controls = new OrbitControls(camera, renderer.domElement);
-// }
 
 // Renderize a cena
 function animate() {
     requestAnimationFrame(animate);
-    // Aqui poderíamos adicionar algum código para atualizar a cena, adicionando algum movimento automático
-    // object.rotation.y += 0.01;
-    // Faça o olho se mover
-    if (object && objToRender === "eye") {
-        // Eu ajustei as constantes aqui até que parecesse bom
-        object.rotation.y = -3 + mouseX / window.innerWidth * 3;
-        object.rotation.x = -1.2 + mouseY * 2.5 / window.innerHeight;
-    }
+    // object.rotation.y += 0.001;
+    // object.position.x = 0;
+    // object.position.y = -0.8;
+    object.rotation.y += 0.001;
+    object.position.x = 1;
+    object.position.y = -0.6;
     renderer.render(scene, camera);
 }
 
@@ -100,9 +85,5 @@ window.addEventListener("resize", function () {
     renderer.setSize(window.innerWidth, window.innerHeight);
 });
 
-document.onmousemove = (e) => {
-    mouseX = e.clientX;
-    mouseY = e.clientY;
-}
 
 animate();
