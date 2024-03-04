045a930474dbc31137aaba20ec2d7ce9827ef0e8
Johnviti <jvsa5@aluno.ifal.edu.br>
2024-03-04 13:55:07 -0300

    Feat: adicionando filtro e alterando js do swiper

--- Assets/Js/AcaoFiltro.js
+++ Assets/Js/AcaoFiltro.js
@@ -2,20 +2,24 @@ const btns = document.querySelectorAll('.btn-project a');
 
 btns.forEach(btn => {
     btn.addEventListener('click', function(event) {
-        event.preventDefault();
         const filter = btn.getAttribute('data-filter');
-        console.log(filter);
+        swiperCards.slideTo(0);
+        event.preventDefault();
+        if (filter === 'all') {
+            swiperCards.forEach(function(slide) {
+                slide.style.display = 'block';
+            });
+        }else{
+            swiperCards.forEach(function(slide) {
+                if (slide.classList.contains(filter)) {
+                    slide.style.display = 'block';
+                }else{
+                    slide.style.display = 'none';
+                }
+            });
+        }
         btns.forEach(btn => btn.classList.remove('active'));
         btn.classList.add('active');
-
-        const cards = document.querySelectorAll('.card__article');
-        cards.forEach(card => {
-            card.style.display = 'none';
-            if (filter === 'all' || card.classList.contains(filter)) {
-                card.style.display = 'block'; 
-                console.log(card);
-            }
-        });
         swiperCards.update();
     });
 });
