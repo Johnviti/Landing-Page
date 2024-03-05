15c567c9eaee40b4e533c10f8cae6451152cc85d
Johnviti <jvsa5@aluno.ifal.edu.br>
2024-03-05 13:22:06 -0300

    Feat: ajuste scroll

--- Assets/Js/AcaoScroll.js
+++ Assets/Js/AcaoScroll.js
@@ -2,10 +2,13 @@ window.addEventListener('scroll', function(){
     var scroll = document.querySelector('.ri-arrow-up-s-line');
     scroll.classList.toggle("active", window.scrollY > 300)
     var aboutMeSection = document.getElementById('sobre');
+    var projetosSection = document.getElementById('projetos');
     if (aboutMeSection.getBoundingClientRect().top + 30 < window.innerHeight) {
-        console.log('entrou');
         scroll.classList.add('dark');
     } else {
+        
+    }
+    if (projetosSection.getBoundingClientRect().top + 30 < window.innerHeight) {
         scroll.classList.remove('dark');
     }
 });
