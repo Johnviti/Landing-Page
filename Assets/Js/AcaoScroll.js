5e3b4766c8aad8267fa43d55f4c5a233138229db
John Victor <84483840+Johnviti@users.noreply.github.com>
2024-03-05 14:47:34 -0300

    Feat: Alteração ico

--- Assets/Js/AcaoScroll.js
+++ Assets/Js/AcaoScroll.js
@@ -1,18 +1,3 @@
-window.addEventListener('scroll', function(){
-    var scroll = document.querySelector('.ri-arrow-up-s-line');
-    scroll.classList.toggle("active", window.scrollY > 300)
-    var aboutMeSection = document.getElementById('sobre');
-    var projetosSection = document.getElementById('projetos');
-    if (aboutMeSection.getBoundingClientRect().top + 30 < window.innerHeight) {
-        scroll.classList.add('dark');
-    } else {
-        
-    }
-    if (projetosSection.getBoundingClientRect().top + 30 < window.innerHeight) {
-        scroll.classList.remove('dark');
-    }
-});
-
 function backToSection(){
     window.scrollTo(
     top = 0,
