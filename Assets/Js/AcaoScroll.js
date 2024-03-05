098db2401c857de30c5239a962308e1d95d58336
Johnviti <jvsa5@aluno.ifal.edu.br>
2024-03-05 13:04:44 -0300

    Feat: Alterar cor do button scroll para preto no fundo branco

--- Assets/Js/AcaoScroll.js
+++ Assets/Js/AcaoScroll.js
@@ -1,7 +1,13 @@
-
 window.addEventListener('scroll', function(){
     var scroll = document.querySelector('.ri-arrow-up-s-line');
-    scroll.classList.toggle("active", window.scrollY > 800)
+    scroll.classList.toggle("active", window.scrollY > 300)
+    var aboutMeSection = document.getElementById('sobre');
+    if (aboutMeSection.getBoundingClientRect().top + 30 < window.innerHeight) {
+        console.log('entrou');
+        scroll.classList.add('dark');
+    } else {
+        scroll.classList.remove('dark');
+    }
 });
 
 function backToSection(){
@@ -10,4 +16,3 @@ function backToSection(){
     behavior = "smooth"    
     );
 }
-
